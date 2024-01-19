610d6ad0540e0ef728cdbd1961f4ae5cd617e35e
Swan <swan.blanc@infomaniak.com>
2024-01-19 15:31:06 +0100

    Add CM2 Add hack prompt poc filter

---
+++ cours/TP/lstm_translate.py
@@ -0,0 +1,129 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim, emb_dim, hid_dim, n_layers, dropout):
+        super().__init__()
+        self.hid_dim = hid_dim
+        self.n_layers = n_layers
+        self.embedding = nn.Embedding(input_dim, emb_dim)
+        self.rnn = nn.LSTM(emb_dim, hid_dim, n_layers, dropout=dropout)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, src):
+        embedded = self.dropout(self.embedding(src))
+        outputs, (hidden, cell) = self.rnn(embedded)
+        return hidden, cell
+
+
+class Decoder(nn.Module):
+    def __init__(self, output_dim, emb_dim, hid_dim, n_layers, dropout):
+        super().__init__()
+        self.output_dim = output_dim
+        self.hid_dim = hid_dim
+        self.n_layers = n_layers
+        self.embedding = nn.Embedding(output_dim, emb_dim)
+        self.rnn = nn.LSTM(emb_dim, hid_dim, n_layers, dropout=dropout)
+        self.fc_out = nn.Linear(hid_dim, output_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, input, hidden, cell):
+        input = input.unsqueeze(0)
+        embedded = self.dropout(self.embedding(input))
+        output, (hidden, cell) = self.rnn(embedded, (hidden, cell))
+        prediction = self.fc_out(output.squeeze(0))
+        return prediction, hidden, cell
+
+
+class Seq2Seq(nn.Module):
+    def __init__(self, encoder, decoder, device):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.device = device
+
+    def forward(self, src, trg, teacher_forcing_ratio=0.5):
+        trg_len = trg.shape[0]
+        batch_size = trg.shape[1]
+        trg_vocab_size = self.decoder.output_dim
+        outputs = torch.zeros(trg_len, batch_size, trg_vocab_size).to(self.device)
+        hidden, cell = self.encoder(src)
+        input = trg[0, :]
+
+        for t in range(1, trg_len):
+            output, hidden, cell = self.decoder(input, hidden, cell)
+            outputs[t] = output
+            teacher_force = random.random() < teacher_forcing_ratio
+            top1 = output.argmax(1)
+            input = trg[t] if teacher_force else top1
+
+        return outputs
+
+
+def train(model, iterator, optimizer, criterion, clip):
+    model.train()
+    epoch_loss = 0
+    for i, batch in enumerate(iterator):
+        src = batch.src
+        trg = batch.trg
+        optimizer.zero_grad()
+        output = model(src, trg)
+        output_dim = output.shape[-1]
+        output = output[1:].view(-1, output_dim)
+        trg = trg[1:].view(-1)
+        loss = criterion(output, trg)
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+        optimizer.step()
+        epoch_loss += loss.item()
+
+    return epoch_loss / len(iterator)
+
+N_EPOCHS = 10
+CLIP = 1
+
+optimizer = optim.Adam(model.parameters())
+criterion = nn.CrossEntropyLoss()
+
+for epoch in range(N_EPOCHS):
+    train_loss = train(model, train_iterator, optimizer, criterion, CLIP)
+    print(f'Epoch: {epoch+1:02} | Train Loss: {train_loss:.3f}')
+
+
+# INFERENCE
+
+def translate_sentence(sentence, tokenizer, model, device, max_length=50):
+    model.eval()
+
+    if isinstance(sentence, str):
+        tokens = ['<sos>'] + tokenizer(sentence) + ['<eos>']
+    else:
+        tokens = ['<sos>'] + [token.lower() for token in sentence] + ['<eos>']
+
+    text_to_indices = [TEXT.vocab.stoi[token] for token in tokens]
+    sentence_tensor = torch.LongTensor(text_to_indices).unsqueeze(1).to(device)
+
+    outputs = [TEXT.vocab.stoi["<sos>"]]
+    for _ in range(max_length):
+        trg_tensor = torch.LongTensor(outputs).unsqueeze(1).to(device)
+
+        with torch.no_grad():
+            output = model(sentence_tensor, trg_tensor)
+
+        best_guess = output.argmax(2)[-1, :].item()
+        outputs.append(best_guess)
+
+        if best_guess == TEXT.vocab.stoi["<eos>"]:
+            break
+
+    translated_sentence = [TEXT.vocab.itos[idx] for idx in outputs]
+    return translated_sentence[1:]
+
+# Exemple d'utilisation:
+sentence = "Je vais bien"
+model = ... # le modèle formé
+device = ... # le dispositif sur lequel nous travaillons
+
+output = translate_sentence(sentence, tokenize_fr, model, device)
